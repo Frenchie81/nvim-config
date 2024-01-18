d20beccbaca4a8da8ed10bccddd0b19fe391e211
Brian French <bfrench9481@gmail.com>
2024-01-18 08:51:38 -0500

    Add hcl treesitter support

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,7 +5,7 @@ return {
     local configs = require("nvim-treesitter.configs")
 
     configs.setup({
-        ensure_installed = { "lua", "javascript", "html", "c_sharp", "rust", "terraform", "json" },
+        ensure_installed = { "lua", "javascript", "html", "c_sharp", "rust", "terraform", "hcl", "json" },
         sync_install = false,
         highlight = { enable = true },
         indent = { enable = true },
