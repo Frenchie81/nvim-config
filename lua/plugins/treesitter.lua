c386e6d67b0a550007af6218c55f26febe4b6bff
Brian French <bfrench9481@gmail.com>
2024-01-19 23:33:30 -0500

    add common treesitter parsers.

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,7 +5,7 @@ return {
     local configs = require("nvim-treesitter.configs")
 
     configs.setup({
-        ensure_installed = { "lua", "javascript", "html", "c_sharp", "rust", "terraform", "hcl", "json" },
+        ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "javascript", "html", "c_sharp", "rust", "terraform", "hcl", "json" },
         sync_install = false,
         highlight = { enable = true },
         indent = { enable = true },
