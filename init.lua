eb05ecce404cc7aa0b0ef2c356772ecdf5c6df3e
Brian French <bfrench9481@gmail.com>
2025-09-29 21:36:18 -0400

    feat: Add session management, bufferline, and diagnostics

--- init.lua
+++ init.lua
@@ -1,23 +1,34 @@
 -- [[ lazy.nvim setup ]]
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
 -- [[ Basic Keymaps ]]
 vim.g.mapleader = " " -- Make sure to set `mapleader` before lazy so your mappings are correct
-vim.keymap.set('n', '<leader>qq', ':qa<CR>', { desc = 'Quit all windows' })
-vim.keymap.set('n', '<C-s>', ':w<CR>', { desc = 'Save buffer' })
-vim.keymap.set('n', '<S-l>', ':bnext<CR>', { desc = 'Next buffer' })
-vim.keymap.set('n', '<S-h>', ':bprevious<CR>', { desc = 'Previous buffer' })
+vim.keymap.set("n", "<leader>qq", ":qa<CR>", { desc = "Quit all windows" })
+vim.keymap.set("n", "<C-s>", ":w<CR>", { desc = "Save buffer" })
+vim.keymap.set("n", "<S-l>", ":bnext<CR>", { desc = "Next buffer" })
+vim.keymap.set("n", "<S-h>", ":bprevious<CR>", { desc = "Previous buffer" })
+
+-- persistence.nvim
+vim.keymap.set("n", "<leader>qs", function()
+    require("persistence").load()
+end, { desc = "Restore last session for current directory" })
+vim.keymap.set("n", "<leader>ql", function()
+    require("persistence").load()
+end, { desc = "Restore last session" })
+vim.keymap.set("n", "<leader>qd", function()
+    require("persistence").stop()
+end, { desc = "Don't save current session" })
 
 -- [[ Basic Options ]]
 vim.opt.number = true
@@ -30,23 +41,24 @@ vim.opt.updatetime = 250
 vim.opt.signcolumn = "yes"
 vim.opt.scrolloff = 8
 vim.opt.sidescrolloff = 8
+vim.diagnostic.config({ virtual_text = true })
 
 -- [[ Basic Autocmds ]]
 -- Highlight when yanking (copying) text
 vim.api.nvim_create_autocmd("TextYankPost", {
-  desc = "Highlight when yanking (copying) text",
-  group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
+    desc = "Highlight when yanking (copying) text",
+    group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+    callback = function()
+        vim.highlight.on_yank()
+    end,
 })
 
 -- [[ lazy.nvim plugins ]]
 require("lazy").setup({
-  spec = {
-    { import = "plugins" },
-  },
-  -- configure any options here
+    spec = {
+        { import = "plugins" },
+    },
+    -- configure any options here
 })
 
 -- The line beneath this is called `modeline`. See `:help modeline`
---
+++ lua/plugins/persistence.lua
@@ -0,0 +1,7 @@
+return {
+  "folke/persistence.nvim",
+  event = "BufReadPre",
+  opts = {
+    -- Options passed to the plugin
+  },
+}
