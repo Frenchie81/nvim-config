17425ec2f844a3c6004518177dc84ce2a5ec0909
Brian French <bfrench9481@gmail.com>
2025-10-06 10:00:59 -0400

    add a binding to disable formatting

--- lua/plugins/formatter.lua
+++ lua/plugins/formatter.lua
@@ -1,3 +1,4 @@
+local format_on_save_enabled = true
 return {
   "stevearc/conform.nvim",
   event = { "BufWritePre" },
@@ -10,6 +11,25 @@ return {
       end,
       mode = "n",
       desc = "Format buffer",
+    },
+    {
+      "<leader>uf",
+      function()
+        format_on_save_enabled = not format_on_save_enabled
+        require("conform").setup({
+          format_on_save = format_on_save_enabled and {
+            timeout_ms = 5000,
+            lsp_fallback = true,
+          } or nil,
+        })
+        if format_on_save_enabled then
+          print("Conform formatting on save enabled")
+        else
+          print("Conform formatting on save disabled")
+        end
+      end,
+      mode = "n",
+      desc = "Toggle format on save",
     },
   },
   opts = {
