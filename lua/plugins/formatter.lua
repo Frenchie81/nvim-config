a506be5350003958e69ebdd641c415dc4044777a
Brian French <bfrench9481@gmail.com>
2025-10-06 09:54:02 -0400

    Add keybind for code format

--- lua/plugins/formatter.lua
+++ lua/plugins/formatter.lua
@@ -2,6 +2,16 @@ return {
   "stevearc/conform.nvim",
   event = { "BufWritePre" },
   cmd = { "ConformInfo" },
+  keys = {
+    {
+      "<leader>cf",
+      function()
+        require("conform").format({ async = true, lsp_fallback = true })
+      end,
+      mode = "n",
+      desc = "Format buffer",
+    },
+  },
   opts = {
     formatters_by_ft = {
       lua = { "stylua" },
