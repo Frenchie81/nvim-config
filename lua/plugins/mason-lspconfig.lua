593f6d8c20301208080c52a7a236d4523eef5205
Brian French <bfrench9481@gmail.com>
2026-03-16 14:33:01 -0400

    add rustacean nvim

--- lua/plugins/mason-lspconfig.lua
+++ lua/plugins/mason-lspconfig.lua
@@ -6,7 +6,7 @@ return {
   },
   config = function()
     require("mason-lspconfig").setup({
-      ensure_installed = { "lua_ls", "rust_analyzer", "html", "terraformls" },
+      ensure_installed = { "lua_ls", "html", "terraformls" },
     })
   end,
 }
--- lua/plugins/neo-test.lua
+++ lua/plugins/neo-test.lua
@@ -63,6 +63,7 @@ return {
             type = "coreclr",
           },
         }),
+        require("rustaceanvim.neotest"),
       },
     })
   end,
