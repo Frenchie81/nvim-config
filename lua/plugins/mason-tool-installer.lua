464166e06dbbc3fa82ee6aa0f29a0774aaa4de28
Brian French <bfrench9481@gmail.com>
2026-05-11 19:11:24 -0400

    only pin roslyn if its linux

--- lua/plugins/mason-tool-installer.lua
+++ lua/plugins/mason-tool-installer.lua
@@ -4,12 +4,17 @@ return {
     "mason-org/mason.nvim",
   },
   config = function()
+    local roslyn = { "roslyn", version = "5.4.0-2.26175.10" }
+    if vim.fn.has("win32") == 1 then
+      roslyn = { "roslyn" }
+    end
+
     require("mason-tool-installer").setup({
       ensure_installed = {
         "csharpier",
         "netcoredbg",
         "prettier",
-        { "roslyn", version = "5.4.0-2.26175.10" },
+        roslyn,
         "stylua",
         "tree-sitter-cli",
         "terraform",
